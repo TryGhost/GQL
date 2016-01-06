b3ec3c42238ff9de27e10330152558ec12ef719a
Austin Burdine <acburdine@gmail.com>
2016-01-05 20:50:24 -0700

    refactor tasks
    closes #10
    - add linting via jshint and jscs (copying files from TryGhost/Ghost)
    - remove unnecessary usage of grunt
    - fix linting errors

--- test/gql_spec.js
+++ test/gql_spec.js
@@ -1,19 +1,20 @@
-var should = require('should'),
-    gql = require('../lib/gql'),
-    knex = require('knex')({});
+/* globals describe, it */
+var gql = require('../lib/gql'),
+    knex = require('knex')({}),
+    toSQL;
 
-var toSQL = exports.toSQL = function (input, resource) {
+toSQL = exports.toSQL = function (input, resource) {
     var parsedFilter = gql.parse(input);
     return gql.knexify(knex(resource), parsedFilter).toQuery();
 };
 
 describe('GQL', function () {
     it('should correctly get from GQL -> SQL', function () {
-        toSQL("id:1", 'posts').should.eql('select * from "posts" where "posts"."id" = 1');
+        toSQL('id:1', 'posts').should.eql('select * from "posts" where "posts"."id" = 1');
     });
 
     it('should correctly escape bad sequences', function () {
-        (function () {toSQL("id:'1 and 1‘=\'1`'", 'posts')}).should.throw();
-        toSQL("id:'1 and 1‘=\\\'1`'", 'posts').should.eql('select * from "posts" where "posts"."id" = \'1 and 1‘=\\\'1`\'');
+        (function () {toSQL('id:\'1 and 1‘=\'1`\'', 'posts');}).should.throw();
+        toSQL('id:\'1 and 1‘=\\\'1`\'', 'posts').should.eql('select * from "posts" where "posts"."id" = \'1 and 1‘=\\\'1`\'');
     });
 });
